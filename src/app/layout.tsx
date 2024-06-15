6e7cb580219200ed1972a8e8601d2c5c20d95f4b
Aleks Rydzkowska <rydzkowska@gmail.com>
2024-06-15 21:17:51 +0100

    added colors and fonts to tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
-import { inter } from "@/lib/fonts";
 import "./globals.css";
-
 
 export const metadata: Metadata = {
   title: "Plaeen",
-  description: "A streamlined and intuitive scheduler tailored for busy gamers, by gamers. Estimate completion dates, share your calendars and conquer levels together!",
+  description:
+    "A streamlined and intuitive scheduler tailored for busy gamers, by gamers. Estimate completion dates, share your calendars and conquer levels together!",
 };
 
 export default function RootLayout({
@@ -15,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
