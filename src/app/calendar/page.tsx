3fc4a70e9cb1bc59bffda73f280a31038288831a
Aleks Rydzkowska <rydzkowska@gmail.com>
2024-06-16 22:22:02 +0100

    corrected frosted glass background

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
   return (
     <div className="relative min-h-screen bg-calendar-bg bg-cover bg-center flex justify-center items-center">
       <div className="absolute inset-0 bg-black opacity-85"></div>
-      <div className="relative w-4/5 h-4/5 bg-lightPurple bg-opacity-15 backdrop-filter  backdrop-blur-lg rounded-lg block p-10">
+      <div className="relative w-4/5 h-4/5 bg-lightPurple bg-opacity-10 backdrop-filter backdrop-blur brightness-125 rounded-lg block p-10">
         <div>
           <h1 className="text-6xl text-green font-abolition justify-left">
             Calendar
