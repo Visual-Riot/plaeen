bcb8bdd341f829e3694a8789c05854e528b4b887
LeanneCodes <leannegoldsmith95@yahoo.com>
2024-06-16 17:24:16 +0100

    landing page complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,21 @@
-import Image from "next/image";
+'use client';
+
+import GreenButton from "@/components/buttons/GreenButton";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <h1 className="text-5xl font-semibold">Plaeen Landing Page</h1>
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <Link href={"/login"}>
-          <h2 className="mb-3 text-2xl font-semibold">
-            Login{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Test login link page.
-          </p>
-        </Link>
-
-        <Link href={"/signup"}>
-          <h2 className="mb-3 text-2xl font-semibold">
-            Sign Up{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Test sign up page
-          </p>
-        </Link>
-
-        <Link href={"/session"}>
-          <h2 className="mb-3 text-2xl font-semibold">
-            Session{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Test Session page
-          </p>
-        </Link>
+    <main>
+      <div className="h-screen w-screen bg-[url('/img/hero-bg.webp')] bg-cover bg-center">
+        <div className="relative top-[203px] space-y-5 left-[139px]">
+          <h1 className="text-neonGreen font-semibold font-sofia text-[64px]">Unleash the power of<br />plaeen together.</h1>
+          <p className="text-lightGrey font-sofia text-[16px]">Life gets busy, but gaming with friends shouldn't be. Plaeen makes scheduling sessions<br />
+          effortless, so you can spend more time playing and less time planning.</p>
+          <div>
+            <input type="email" placeholder="john.smith@gmail.com" className="border-neonGreen border-2 h-[64px] w-[408px] p-5 rounded-md bg-transparent"/>
+            <GreenButton onClick={() => console.log('Button clicked!')}>Get started</GreenButton>
+          </div>
+        </div>
       </div>
     </main>
   );
