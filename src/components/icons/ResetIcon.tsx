8d8b0675adb6e5ee150d8c48a6289f8b20d582f6
Kane Chang <kane.chang.fl@gmail.com>
2024-06-25 19:15:36 +0100

    Type error fix for vercel deployment on ResetIcon.tsx
    added type string for className

--- src/components/icons/ResetIcon.tsx
+++ src/components/icons/ResetIcon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ResetIcon = ({ className }) => (
+const ResetIcon = ({ className }: {className: string}) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="14.641"
