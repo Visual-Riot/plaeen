55edd9e51a5bceb0d223d54fc2672f2c37176cce
Kane Chang <kane.chang.fl@gmail.com>
2024-06-30 12:46:41 +0100

    extracted Window API into custom hook

--- src/components/calendar/PlayerCalendarWrapper.tsx
+++ src/components/calendar/PlayerCalendarWrapper.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import PlayerCalendarDesktop from "./PlayerCalendarDesktop";
 import PlayerCalendarMobile from "./PlayerCalendarMobile";
+import useDeviceSize from "@/lib/hooks";
 
 interface PlayerCalendarWrapperProps {
   dayHours: { [key: string]: { [key: number]: string } };
@@ -15,7 +16,8 @@ const PlayerCalendarWrapper: React.FC<PlayerCalendarWrapperProps> = ({
   dayHours,
   setDayHours,
 }) => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
+  const [width, height] = useDeviceSize()
+  const [isMobile, setIsMobile] = useState(width <= 1024);
   const [selectedDay, setSelectedDay] = useState<string>("Monday");
 
   useEffect(() => {
@@ -31,7 +33,7 @@ const PlayerCalendarWrapper: React.FC<PlayerCalendarWrapperProps> = ({
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 1024);
+      setIsMobile(width <= 1024);
     };
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
