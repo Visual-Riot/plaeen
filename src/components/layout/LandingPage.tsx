c2421faa61d41f557883f8c0e363d5ecf523ce47
Leanne Goldsmith <leannegoldsmith@Leannes-MacBook-Pro.local>
2024-07-14 14:43:01 +0100

    additional font-sizing changes to heading on landing page

--- src/components/layout/LandingPage.tsx
+++ src/components/layout/LandingPage.tsx
@@ -9,8 +9,8 @@ const LandingPage = () => {
     <div className="h-[85vh] w-screen bg-[url('/img/hero-bg-noshadow.webp')] bg-cover bg-bottom mb-[16rem]">
       <HomeNavbar />
       <div className="relative top-[0%] 2xl:top-[10%] space-y-5 px-6 sm:px-12 md:px-20 lg:px-32">
-        <h1 className="text-neonGreen font-semibold font-sofia landing-heading">
-          Unleash the power of<br />plaeen together.
+        <h1 className="text-neonGreen font-semibold font-sofia landing-heading w-full md:w-[90%] xl:w-[55%]">
+          Unleash the power of plaeen together.
         </h1>
         <p className="text-lightGrey font-sofia font-extralight landing-paragraph py-3 w-full md:w-[80%] xl:w-[50%]">
           Life gets busy, but gaming with friends shouldn't be. Plaeen makes scheduling sessions
