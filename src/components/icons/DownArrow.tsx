a1c99ccea35e6fa9c545663d4d6ab769807795ee
Leanne Goldsmith <leannegoldsmith@Leannes-MacBook-Pro.local>
2024-07-14 13:28:25 +0100

    green triangle has rounded corners

--- src/components/icons/DownArrow.tsx
+++ src/components/icons/DownArrow.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BiSolidDownArrow } from "react-icons/bi";
+import { TbTriangleInvertedFilled } from "react-icons/tb";
 
 const DownArrow: React.FC = () => {
   return (
     <div className="flex justify-center my-10 xxs:mt-[-240px] sm:mt-[-225px] lg:mt-[-200px]">
-      <BiSolidDownArrow className='text-neonGreen w-6 h-6 animate-bounce' />
+      <TbTriangleInvertedFilled className='text-neonGreen w-6 h-6 animate-bounce' />
     </div>
   );
 };
