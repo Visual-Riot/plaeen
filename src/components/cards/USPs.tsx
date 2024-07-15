b2a46337a6d2436928cb7081bf157a365e4c7750
Leanne Goldsmith <leannegoldsmith@Leannes-MacBook-Pro.local>
2024-07-15 12:42:15 +0100

    changed usp img to Image, corrected text readability on landing page and converted css styling to tailwind classes

--- src/components/cards/USPs.tsx
+++ src/components/cards/USPs.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 // Assuming Icon can be a component or a string URL
@@ -11,7 +12,7 @@ type USPsProps = {
 const USPs: React.FC<USPsProps> = ({ icon, headline, text, className = '' }) => {
     return (
         <div className={`relative text-left p-7 lg:p-10 mx-5 mb-5 shadow-lg rounded-lg w-full lg:w-1/4 min-h-[100px] lg:min-h-[300px] ${className}`}>
-            <img src={icon} alt="Icon" className="mb-4 w-18 h-auto left-0" />
+            <Image src={icon} alt="Icon" className="mb-4 left-0" width={70} height={70} />
             <h3 className="text-[22px] my-6 text-white font-medium font-sofia">{headline}</h3>
             <p className="text-[16px] text-lightGrey font-sofia font-extralight leading-7">{text}</p>
         </div>
--- src/components/layout/LandingPage.tsx
+++ src/components/layout/LandingPage.tsx
@@ -1,20 +1,18 @@
-"use client";
+'use client';
 
-import GreenButton from "@/components/buttons/GreenButton";
-import HomeNavbar from "@/components/layout/HomeNavbar";
-import '../../styles/LandingPage.css';
+import GreenButton from '@/components/buttons/GreenButton';
+import HomeNavbar from '@/components/layout/HomeNavbar';
 
 const LandingPage = () => {
   return (
     <div className="h-[85vh] w-screen bg-[url('/img/hero-bg-noshadow.webp')] bg-cover bg-bottom mb-[16rem]">
       <HomeNavbar />
       <div className="relative top-[0%] 2xl:top-[10%] space-y-5 px-6 sm:px-12 md:px-20 lg:px-32">
-        <h1 className="text-neonGreen font-semibold font-sofia landing-heading w-full md:w-[90%] xl:w-[55%]">
-          Unleash the power of plaeen together.
+        <h1 className="text-neonGreen font-semibold font-sofia landing-heading w-full md:w-[90%] xl:w-[55%] text-[70px] leading-normal max-[1580px]:text-[65px] max-[1280px]:text-[60px] max-[780px]:text-[50px] max-[500px]:text-[45px]">
+          {"Unleash the power of plaeen together."}
         </h1>
-        <p className="text-lightGrey font-sofia font-extralight landing-paragraph py-3 w-full md:w-[80%] xl:w-[50%]">
-          Life gets busy, but gaming with friends shouldn't be. Plaeen makes scheduling sessions
-          effortless, so you can spend more time playing and less time planning.
+        <p className="landing-paragraph text-lightGrey font-sofia font-extralight py-3 w-full md:w-[80%] xl:w-[50%] text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl leading-[1.6]">
+          {"Life gets busy, but gaming with friends shouldn't be. Plaeen makes scheduling sessions effortless, so you can spend more time playing and less time planning."}
         </p>
         <div className="flex flex-col sm:flex-row items-center xs:items-start space-y-4 lg:space-y-0 lg:space-x-4">
           <input
@@ -22,7 +20,7 @@ const LandingPage = () => {
             placeholder="john.smith@gmail.com"
             className="border-neonGreen border-2 mt-4 lg:mt-0 h-12 xs:h-[64px] xs:w-full sm:w-[250px] md:w-[408px] p-5 rounded-lg bg-transparent"
           />
-          <GreenButton onClick={() => console.log("Button clicked!")}>
+          <GreenButton onClick={() => console.log('Button clicked!')}>
             Get started
           </GreenButton>
         </div>
