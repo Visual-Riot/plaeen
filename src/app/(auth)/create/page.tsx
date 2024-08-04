034f2e128fe22e23a50ca85f6b8bc732990b1ef3
Leanne Goldsmith <leannegoldsmith@Leannes-MacBook-Pro.local>
2024-08-04 19:40:23 +0100

    add contents for create your character page

--- src/app/(auth)/create/page.tsx
+++ src/app/(auth)/create/page.tsx
@@ -2,52 +2,55 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { FcGoogle } from "react-icons/fc";
+import { TiPlus } from "react-icons/ti";
 import Navbar from "@/components/layout/Navbar";
+import GreenButton from "@/components/buttons/GreenButton";
 
 export default function Page() {
   return (
     <>
-    <Navbar />
-    <div className="bg-black w-full h-[100vh] bg-[url('/img/bg-img_01.webp')] bg-cover bg-center">
-    <div className="bg-[black]/85 w-full h-[100vh] flex flex-col items-center">
-    <div className="bg-[#6606E3]/5 w-full h-[100vh] flex flex-col items-center">
+      <Navbar />
+      <div className="bg-black w-full h-full bg-[url('/img/bg-img_01.webp')] bg-cover bg-center">
+        <div className="bg-[black]/85 w-full h-screen flex flex-col items-center justify-center mt-[-70px]">
+          <div className="bg-[#6606E3]/5 w-full flex flex-col items-center justify-center h-full">
 
-      {/* Logo */}
-      <div className="flex justify-center">
-        <Image
-          src="logo\logo-icon.svg"
-          alt="Plaeen logo"
-          width={70}
-          height={52}
-          className="py-11"
-        />
+            {/* Log in / Sign in Card */}
+            <div className="p-16 bg-lightPurple/15 min-w-[320px] md:min-w-[520px] rounded-lg backdrop-blur-[14px] backdrop-brightness-[1.4] mt-[60px]">
+
+              {/* Create Character Section */}
+              <div>
+                <h2 className="text-white font-semibold text-3xl mt-0.5">
+                  Create your character
+                </h2>
+                <p className="font-light text-white text-sm mt-8">Add your username</p>
+                <input
+                  type="text"
+                  placeholder="username"
+                  className="border-green bg-green opacity-75 border-2 mt-4 w-full p-5 rounded-lg"
+                />
+              </div>
+              <div className="flex mt-8 items-center">
+                <Link href="/">
+                    <div className="border border-taupe bg-taupe opacity-35 rounded-full w-[77px] h-[77px] flex items-center justify-center text-white text-2xl">
+                        <TiPlus />
+                    </div>
+                </Link>
+                
+                <Link href="/" className="text-green underline ml-5">Upload avatar</Link>
+              </div>
+              <div>
+                <GreenButton onClick={() => console.log('clicked')} className="mt-8 p-[1.25rem!important] text-black w-full">
+                  Continue
+                </GreenButton>
+              </div>
+              <div>
+                <Link href="/" className="text-white underline text-sm flex justify-center mt-8 hover:text-gray-300">Skip for now</Link>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
-
-      {/* Log in / Sign in Card */}
-        <div className="p-16 bg-lightPurple/15 min-w-[520px] rounded-lg backdrop-blur-[14px] backdrop-brightness-[1.4]">
-
-        {/* Google OAuth Section */}
-        <div>
-          <h2 className="text-white font-semibold text-3xl mt-0.5">
-            Log in or Sign up
-          </h2>
-          <p className="text-lightGrey font-light text-lg mt-2.5">Get started for free</p>
-          <button onClick={()=> console.log('clicked')}className="text-lightGrey text-lg mt-8 border-4 border-green rounded-[10px] w-full p-5 border-opacity-50 flex items-center justify-center gap-7">
-            <FcGoogle size={24} />
-            Continue with Google
-          </button>
-        </div>
-
-        <div className="my-8 flex basis-full text-lightGrey font-thin before:bg-[#707070] before:h-px before:grow before:mr-4 before:mt-2 after:bg-[#707070] after:h-px after:grow items-center after:ml-4 after:mt-2">
-          or
-        </div>
-
-        </div>
-
-        </div>
-        </div>
-    </div>
     </>
   );
 }
