aa5a185dcf33362c9a722f3a36a383157a125dca
Kane Chang <kane.chang.fl@gmail.com>
2024-08-19 17:14:34 +0100

    updated auth adapter to work with Prisma

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth";
 import { options } from "./auth.config";
+import { db } from "./lib/db";
+import { PrismaAdapter } from "@auth/prisma-adapter"
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
     // TODO decide on session strategy
+    adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
     ...options
   });
