733827bf6da4c7b1d3bce501c48437140952c3da
Kane Chang <kane.chang.fl@gmail.com>
2024-08-19 17:56:13 +0100

    added auth callbacks by extending sessions to include user id and user role (admin or user)

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,30 @@
 import NextAuth from "next-auth";
-import { options } from "./auth.config";
-import { db } from "./lib/db";
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { options } from "@/auth.config";
+import { db } from "@/lib/db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { getUserById } from "@/lib/data/user";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-    // TODO decide on session strategy
-    adapter: PrismaAdapter(db),
-    session: { strategy: "jwt" },
-    ...options
-  });
-  
+  // TODO decide on session strategy
+  adapter: PrismaAdapter(db),
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as "ADMIN" | "USER";
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+      return token;
+    },
+  },
+  session: { strategy: "jwt" },
+  ...options,
+});
